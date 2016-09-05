facefa2a0413b95324d52fa1e4bcc809fbeb8290
simmonsajr <simmonsajr@gmail.com>
2016-09-05 15:40:33 -0400

    Client Training Commit

--- app/src/main/java/com/bignerdranch/android/clienttraining/AddSession.java
+++ app/src/main/java/com/bignerdranch/android/clienttraining/AddSession.java
@@ -1,15 +1,17 @@
 package com.bignerdranch.android.clienttraining;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Clients extends AppCompatActivity {
+public class AddSession extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clients);
+        setContentView(R.layout.activity_add_session);
     }
 }
--- app/src/main/java/com/bignerdranch/android/clienttraining/ClientList.java
+++ app/src/main/java/com/bignerdranch/android/clienttraining/ClientList.java
@@ -1,15 +1,13 @@
 package com.bignerdranch.android.clienttraining;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Clients extends AppCompatActivity {
+public class ClientList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clients);
+        setContentView(R.layout.activity_client_list);
     }
 }
